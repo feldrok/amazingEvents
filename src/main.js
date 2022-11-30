187c7b4b903eb6a28139678c61bb2a155f35d07c
feldrok <matias@moravalero.cl>
2022-11-30 17:32:06 -0300

    task 1 v1 final final

--- src/main.js
+++ src/main.js
@@ -1,50 +1,50 @@
-currentSlideID = 1;
+currentSlideID = 1
 
-sliderElement = document.getElementById("slider");
-totalSlides = sliderElement.childElementCount;
+sliderElement = document.getElementById("slider")
+totalSlides = sliderElement.childElementCount
 
 function next() {
-    if (currentSlideID < totalSlides) {
-        currentSlideID++;
-        showSlide();
-    } else if (currentSlideID === totalSlides) {
-        currentSlideID = 1;
-        showSlide();
-    }
+  if (currentSlideID < totalSlides) {
+    currentSlideID++
+    showSlide()
+  } else if (currentSlideID === totalSlides) {
+    currentSlideID = 1
+    showSlide()
+  }
 }
 
 function prev() {
-    if (currentSlideID > 1) {
-        currentSlideID--;
-        showSlide();
-    } else if (currentSlideID === 1) {
-        currentSlideID = totalSlides;
-        showSlide();
-    }
+  if (currentSlideID > 1) {
+    currentSlideID--
+    showSlide()
+  } else if (currentSlideID === 1) {
+    currentSlideID = totalSlides
+    showSlide()
+  }
 }
 
 function showSlide() {
-    slides = document.getElementById("slider").getElementsByTagName("li");
-    for (let index = 0; index < totalSlides; index++) {
-        const element = slides[index];
-        if(currentSlideID===index+1) {
-            element.classList.remove("hidden");
-        } else {
-            element.classList.add("hidden");
-        }
+  slides = document.getElementById("slider").getElementsByTagName("li")
+  for (let index = 0; index < totalSlides; index++) {
+    const element = slides[index]
+    if (currentSlideID === index + 1) {
+      element.classList.remove("hidden")
+    } else {
+      element.classList.add("hidden")
     }
+  }
 }
 
 function autoPlay() {
-    showSlide();
-    setTimeout(() => {
-        autoPlay();
-        if (currentSlideID === totalSlides) {
-            currentSlideID = 1;
-        } else {
-            currentSlideID++;
-        }
-    }, 5000);    
+  showSlide()
+  setTimeout(() => {
+    autoPlay()
+    if (currentSlideID === totalSlides) {
+      currentSlideID = 1
+    } else {
+      currentSlideID++
+    }
+  }, 5000)
 }
 
-autoPlay();
+autoPlay()
