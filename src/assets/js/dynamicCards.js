fb169a03ed7479d8bf2c88b5cb48bd4b403a1e3a
feldrok <matias@moravalero.cl>
2022-12-13 12:08:05 -0300

    last commit

--- src/assets/js/dynamicCards.js
+++ src/assets/js/dynamicCards.js
@@ -10,9 +10,9 @@ const containerButton = document.querySelector(".containerCategoryButtons")
 
 const displayCards = eventItems => {
   if (cardsContainerAll) {
-    let displayEvents = eventItems.events.map(function (event) {
+    let displayEvents = eventItems.events.map((event) => {
       return `
-      <div class="xl:w-1/4 md:w-1/2 p-4 w-full h-full" id="card">
+      <div class="xl:w-1/4 md:w-1/2 p-4 w-full h-full active" id="card">
         <div class="bg-white min-h-[450px] dark:bg-tertiary dark:border-gray-200 dark:shadow-black shadow-md border flex flex-col justify-around p-6 rounded-lg hover:scale-105 hover:shadow-none duration-300">
           <img
             class="h-40 rounded w-full object-cover object-center mb-6"
@@ -42,11 +42,11 @@ const displayCards = eventItems => {
     displayEvents = displayEvents.join("")
     cardsContainerAll.innerHTML = displayEvents
   } else if (cardsContainerUpcoming) {
-    let displayEvents = eventItems.events.map(function (event) {
+    let displayEvents = eventItems.events.map((event) => {
       if (event.date > currentDate) {
         return `
-        <div class="xl:w-1/4 md:w-1/2 p-4 w-full h-full" id="card">
-          <div class="bg-white min-h-[450px] dark:bg-tertiary dark:border-gray-200 dark:shadow-black shadow-md border flex flex-col justify-around p-6 rounded-lg hover:scale-105 hover:shadow-none duration-300">
+        <div class="xl:w-1/4 md:w-1/2 p-4 w-full h-full active" id="card">
+        <div class="bg-white min-h-[450px] dark:bg-tertiary dark:border-gray-200 dark:shadow-black shadow-md border flex flex-col justify-around p-6 rounded-lg hover:scale-105 hover:shadow-none duration-300">
             <img
               class="h-40 rounded w-full object-cover object-center mb-6"
               src="${event.image}"
@@ -76,11 +76,11 @@ const displayCards = eventItems => {
     displayEvents = displayEvents.join("")
     cardsContainerUpcoming.innerHTML = displayEvents
   } else if (cardsContainerPast) {
-    let displayEvents = eventItems.events.map(function (event) {
+    let displayEvents = eventItems.events.map((event) => {
       if (event.date < currentDate) {
         return `
-        <div class="xl:w-1/4 md:w-1/2 p-4 w-full h-full" id="card">
-          <div class="bg-white min-h-[450px] dark:bg-tertiary dark:border-gray-200 dark:shadow-black shadow-md border flex flex-col justify-around p-6 rounded-lg hover:scale-105 hover:shadow-none duration-300">
+        <div class="xl:w-1/4 md:w-1/2 p-4 w-full h-full active" id="card">
+        <div class="bg-white min-h-[450px] dark:bg-tertiary dark:border-gray-200 dark:shadow-black shadow-md border flex flex-col justify-around p-6 rounded-lg hover:scale-105 hover:shadow-none duration-300">
             <img
               class="h-40 rounded w-full object-cover object-center mb-6"
               src="${event.image}"
@@ -114,7 +114,7 @@ const displayCards = eventItems => {
 
 const displayCategoryButtons = () => {
   const categories = data.events.reduce(
-    function (values, item) {
+    (values, item) => {
       if (!values.includes(item.category)) {
         values.push(item.category)
       }
@@ -122,7 +122,7 @@ const displayCategoryButtons = () => {
     }, []
   )
   const categoryBtns = categories
-    .map(function (category) {
+    .map((category) => {
       return `
     <li class="flex-1 cursor-pointer justify-center items-center w-full">
     <label class="flex p-4 drop-shadow filterButton h-full justify-center items-center m-3 hover:text-primary-500 dark:hover:text-primary-500 duration-150 text-sm text-gray-600 dark:text-white font-medium cursor-pointer" id="categoryLabel" for="${category}"><input type="checkbox" name="category" class="checkbox" value="${category}" id="${category}">${category}</label>
@@ -133,44 +133,49 @@ const displayCategoryButtons = () => {
   containerButton.innerHTML = categoryBtns
 }
 
-function filterEvents() {
+const filterEvents = () => {
   const searchInput = document.querySelector("#search")
   const filter = searchInput.value.toLowerCase()
   const listCards = document.querySelectorAll("#card")
-
   const checkboxes = document.querySelectorAll('input[type="checkbox"]')
-  const categoryLabel = document.querySelectorAll("#categoryLabel")
   const checked = []
+  
   checkboxes.forEach((checkbox) => {
     if (checkbox.checked) {
       checked.push(checkbox.value)
-
     }
   })
-  listCards.forEach(function (card) {
-    let eventTitle = card.querySelector("#name").textContent.toLowerCase()
-    let eventCategory = card.querySelector("#cardCategory").textContent
-    if (checked.length > 0) {
-      for (let i = 0; i < checked.length; i++) {
-        if (eventCategory.includes(checked[i]) && eventTitle.includes(filter)) {
-          card.style.display = ""
+  
+  if (checked.length > 0) {
+    listCards.forEach((card) => {
+      let eventTitle = card.querySelector("#name").textContent.toLowerCase()
+      let eventCategory = card.querySelector("#cardCategory").textContent
+      checked.forEach((category) => {
+        if (eventCategory.includes(category) && eventTitle.includes(filter)) {
+          card.classList.add("active")
+          card.classList.remove("hidden")
         } else {
-          card.style.display = "none"
+          card.classList.remove("active")
+          card.classList.add("hidden")
         }
-      }
-    } else {
+      })
+    })
+  } else {
+    listCards.forEach((card) => {
+      let eventTitle = card.querySelector("#name").textContent.toLowerCase()
       if (eventTitle.includes(filter)) {
-        card.style.display = ""
+        card.classList.toggle("active", true)
+        card.classList.toggle("hidden", false)
       } else {
-        card.style.display = "none"
+        card.classList.toggle("active", false)
+        card.classList.toggle("hidden", true)
       }
-    }
-  })
-
+    })
+  }
 }
   
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", () => {
   displayCards(data)
   displayCategoryButtons()
   addEventListener('input', filterEvents)
